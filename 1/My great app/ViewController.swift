2d189401772e10615791ac52412b73a75a98a56a
erhatod <47834647+erhatod@users.noreply.github.com>
2020-12-02 22:49:36 +0200

    Add files via upload

---
+++ 1/My great app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My great app
+//
+//  Created by Oleksii Afonin on 29.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
